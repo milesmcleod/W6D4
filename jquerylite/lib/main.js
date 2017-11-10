7d1619c6148b3564be9f760c62b9823b7b04d826
Miles McLeod <milesmcleod123@gmail.com>
2017-11-09 16:08:33 -0800

    Complete AJAX functionality in jQuery lite

--- jquerylite/lib/main.js
+++ jquerylite/lib/main.js
@@ -1,13 +1,43 @@
 const DOMNodeCollection = require("./dom_node_collection.js");
 
 const $l = function (selector) {
-  if(selector instanceof HTMLElement) {
+  if (selector instanceof Function) {
+    document.addEventListener("DOMContentLoaded", selector);
+  } else if(selector instanceof HTMLElement) {
     let newArr = [selector];
     return new DOMNodeCollection(newArr);
   } else {
     const elements = document.querySelectorAll(selector);
     return new DOMNodeCollection(elements);
   }
+};
+
+$l.extend = function (...objects) {
+  let newObject = {};
+  objects.forEach((object) => {
+    let keys = Object.keys(object);
+    keys.forEach((key) => {
+      newObject[`${key}`] = object[`${key}`];
+    });
+  });
+  return newObject;
+};
+
+$l.ajax = function (options) {
+  let defaultObject = {
+    url: "",
+    type: 'GET',
+    contentType: 'JSON',
+    data: {},
+    success (response) {console.log(response);},
+    error (error) {alert(`${error}`);}
+  };
+  let xhrData = $l.extend(defaultObject, options);
+  let xhr = new XMLHttpRequest();
+  xhr.open(xhrData.method, xhrData.url);
+  xhr.onload = xhrData.success;
+  debugger
+  xhr.send(xhrData.data);
 };
 
 window.$l = $l;
